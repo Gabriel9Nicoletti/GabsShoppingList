6316f42cbcc2c9f856b29c42b7e363b3620311e5
luisgdev303 <luisg.dev.lu@gmail.com>
2026-04-29 11:38:06 -0300

    codando

---
+++ GabsShoppingList/app/index.jsx
@@ -0,0 +1,19 @@
+import {Link} from 'expo-router';
+import { StyleSheet, Text, View, image } from 'react-native';
+
+export default function Welcome() {
+  return (
+    <View style={styles.container}>
+    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
